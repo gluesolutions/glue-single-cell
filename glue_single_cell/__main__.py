3a84ac08f27763be34815cdecc6416bf8b9396bb
Jonathan Foster <jonathan.bruce.foster@gmail.com>
2021-12-03 17:14:49 -0500

    Switch to keep adata out of memory

--- glue_single_cell/__main__.py
+++ glue_single_cell/__main__.py
@@ -17,14 +17,16 @@ import anndata
 
 def main(argv):
     #def load_data():
-    df = pd.read_csv('sparse_log_counts_sub_plus_labels.csv')
-    sparse_coo = sparse.coo_matrix((df['x'].values,
-                                   (pd.factorize(df['cell_id'])[0],
-                                    pd.factorize(df['gene'])[0])))
-    sparse_csc = sparse_coo.tocsc()
-    adata = anndata.AnnData(X=sparse_csc)
-    adata.filename = 'test_backfile.h5ad'
-    d = DataAnnData(adata,name="test_name")
+    #df = pd.read_csv('sparse_log_counts_sub_plus_labels2.csv')
+    #sparse_coo = sparse.coo_matrix((df['x'].values,
+    #                               (pd.factorize(df['cell_id'])[0],
+    #                                pd.factorize(df['gene'])[0])))
+    #sparse_csc = sparse_coo.tocsc()
+    #adata = anndata.AnnData(X=sparse_csc)
+    #adata.filename = 'test_backfile_small.h5ad'
+    
+    adata = anndata.read_h5ad('def_sparse.h5ad',backed='r')
+    d = DataAnnData(adata,label="test_name")
     dc = DataCollection([d])
     ga = GlueApplication(dc)
     ga.start()
