8a88d557370396ded27a61b655a45817a2ea7110
Jonathan Foster <jonathan.bruce.foster@gmail.com>
2022-06-13 17:08:34 -0400

    Add basic framework for enrichr

--- glue_single_cell/__init__.py
+++ glue_single_cell/__init__.py
@@ -3,3 +3,5 @@ def setup():
     from .anndata_factory import setup_anndata # noqa
     from .menubar_plugin import diff_gene_exp_plugin  # noqa
     from .menubar_plugin import pca_subset_exp_plugin  # noqa
+    from .menubar_plugin import pca_subset_exp_plugin  # noqa
+    from .menubar_plugin import gseapy_plugin # noqa
--- glue_single_cell/menubar_plugin.py
+++ glue_single_cell/menubar_plugin.py
@@ -2,6 +2,7 @@ from glue.config import menubar_plugin
 
 from .qt.diff_gene_exp import DiffGeneExpDialog
 from .qt.pca_subset import PCASubsetDialog
+from .qt.gsea import GSEApyDialog
 
 
 @menubar_plugin("Scanpy Differential Gene Expression")
@@ -14,4 +15,10 @@ def diff_gene_exp_plugin(session, data_collection):
 def pca_subset_exp_plugin(session, data_collection):
     PCASubsetDialog.summarize(data_collection,
                                     default=None, parent=None)
+    return
+
+@menubar_plugin("Enrich Gene Set via Enrichr")
+def gseapy_plugin(session, data_collection):
+    GSEApyDialog.enrich(data_collection,
+                            default=None, parent=None)
     return
