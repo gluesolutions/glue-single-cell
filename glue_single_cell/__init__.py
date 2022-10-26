33234e574cb96ee5e6f62e884d03b8483331db07
Jonathan Foster <jonathan.bruce.foster@gmail.com>
2022-10-26 16:44:40 -0400

    Add some additional colormaps for mapped-components and categoricals

--- glue_single_cell/__init__.py
+++ glue_single_cell/__init__.py
@@ -8,3 +8,20 @@ def setup():
 
     from glue.config import qt_client
     qt_client.add(QTLViewer)
+
+    # Add colormaps we can use when we use the pca_subset_exp_plugin 
+    # to maps gene expression to a summary over cells. A summary generated 
+    # from a red subset can then be displayed using the 'Reds' colormap
+    # This does not happen automatically... at least not yet
+
+    from glue.config import colormaps
+    import matplotlib.cm as cm
+    colormaps.add('Reds', cm.Reds)
+    colormaps.add('Greens', cm.Greens)
+    colormaps.add('Blues', cm.Blues)
+
+    # Add some categorical colormaps
+
+    colormaps.add('Pastel1', cm.Pastel1)
+    colormaps.add('Paied', cm.Paired)
+    colormaps.add('tab20', cm.tab20)
