a7a7440a5336b3de5a2a0048b49051babda271e2
Jonathan Foster <jonathan.bruce.foster@gmail.com>
2022-11-10 14:18:13 -0500

    Adjust extra colormaps

--- glue_single_cell/__init__.py
+++ glue_single_cell/__init__.py
@@ -14,14 +14,28 @@ def setup():
     # from a red subset can then be displayed using the 'Reds' colormap
     # This does not happen automatically... at least not yet
 
+    d3_dozen = ['#1E77B3','#FF7E0F','#2BA02A','#D62628',
+                  '#9367BC','#8C564B','#E277C1','#7E7E7E',
+                  '#BCBC21','#17BDCF','#3A0182','#004201',]
+    d3_twenty = ['#1E77B3','#FF7E0F','#2BA02A','#D62628',
+                  '#9367BC','#8C564B','#E277C1','#7E7E7E',
+                  '#BCBC21','#17BDCF','#3A0182','#004201',
+                  '#0DFFA8','#5D003F','#BCBCFF','#D8AFA1',
+                  '#B80080','#004D52','#6B6400','#7C0100','#6027FE']
+
     from glue.config import colormaps
     import matplotlib.cm as cm
+    from matplotlib.colors import ListedColormap
     colormaps.add('Reds', cm.Reds)
     colormaps.add('Greens', cm.Greens)
     colormaps.add('Blues', cm.Blues)
-
+    colormaps.add('Purples', cm.Purples)
+    colormaps.add('Oranges', cm.Oranges)
     # Add some categorical colormaps
 
+    colormaps.add('d3_dozen',ListedColormap(d3_dozen, name='d3_dozen'))
+    colormaps.add('d3_twenty',ListedColormap(d3_dozen, name='d3_twenty'))
+
     colormaps.add('Pastel1', cm.Pastel1)
-    colormaps.add('Paied', cm.Paired)
+    colormaps.add('Paired', cm.Paired)
     colormaps.add('tab20', cm.tab20)
